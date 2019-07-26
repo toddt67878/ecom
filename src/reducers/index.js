fb365bbb38926194ea017598686c21e70566f267
Todd Traylor <toddt67878@yahoo.com>
2019-07-25 21:39:20 -0500

    setup reduxform

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,15 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form'; 
+
+import headerNavbar from './headernavbarReducer';
+import user from './userReducer';
+import shop from './shopReducer';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  form,
+  headerNavbar,
+  user,
+  shop
 });
 
 export default rootReducer;
